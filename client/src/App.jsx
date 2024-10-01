85167a298351ab6af55ff5b70cf40f30fed1e331
EgoisticPotato <egoistic.potato@gmail.com>
2024-10-01 09:27:38 +0530

    Changed the UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from './Pages/Home';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <AboutUs />
     </div>
   );
 }
